fce0fcdc623a9eb862299286dead7449a480ac1f
ouchari-ibrahim <ouchariibra@gmail.com>
2026-01-05 10:21:53 +0100

    [IMP] nn_collect_components:  - changed the string to french

--- nn_collect_components/models/stock_move.py
+++ nn_collect_components/models/stock_move.py
@@ -1,26 +1,43 @@
 from odoo import fields, models, api
 
+
 class MoveRawIds(models.Model):
     _inherit = 'stock.move'
 
-    qty_delivered = fields.Float(string="Qty livrée", digits=(16, 4))
-    qty_consumed_total = fields.Float(string="Qty consommée totale", digits=(16, 4))
-    qty_in_prod = fields.Float(string="Qty en production", digits=(16, 4))
+    qty_delivered = fields.Float(
+        string="Qté livrée",
+        digits=(16, 4)
+    )
+
+    qty_consumed_total = fields.Float(
+        string="Qté consommée totale",
+        digits=(16, 4)
+    )
+
+    qty_in_prod = fields.Float(
+        string="Qté en production",
+        digits=(16, 4)
+    )
+
     qty_left = fields.Float(
-        string="Qty Left",digits=(16, 4)
+        string="Qté restante",
+        digits=(16, 4)
     )
+
     qty_needed = fields.Float(
-        string="Qty Needed",
-        compute='_compute_qty_needed',
-        store=False,digits=(16, 4)
+        string="Qté nécessaire",
+        compute="_compute_qty_needed",
+        store=False,
+        digits=(16, 4)
     )
+
     qty_scraped = fields.Float(
-        string="Qty rebut",
+        string="Qté rebutée",
         digits=(16, 4)
     )
 
     qty_returned = fields.Float(
-        string="Quantité retournée",
+        string="Qté retournée",
         digits=(16, 4)
     )
 
